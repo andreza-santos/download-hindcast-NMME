56b421aa3c8eae4839e2edb08fc53da9ca37e58a
andreza-santos <andrezadantast@gmail.com>
2020-12-16 13:19:49 -0300

    atualização tabela

--- R/test-down-nmme.R
+++ R/test-down-nmme.R
@@ -100,13 +100,25 @@ models <- data.table(modelos = c("CanCM4i", "CanSIPSv2", "CMC1-CanCM3",
                      ano_i = c("1981", "1981", "1981",  
                                "1981", "1981", "1981",   
                                "1980", "1982"),  
-                     ano_f = c("2019", "2019", "2011",
-                               "2011", "2019", "2017", 
-                               "2011", "2011")) 
+                     ano_f = c("2018", "2018", "2010",
+                               "2010", "2018", "2017", 
+                               "2010", "2010"),
+                     n_membros = c(10, 20, 10,
+                                  10, 10, 4,
+                                  10, 24),
+                     lead = c(11.5, 11.5, 11.5,
+                              11.5, 11.5, 8.5,
+                              11.5, 9.5)
+)
 
 # ver nomes das variaveis
-# Total Precipitation (mm/day): prec 
-# Sea Surface Temperature (K): sst
+# Total Precipitation (mm/day): prec
+
+# CanCM4i, CanSIPSv2, CMC1-CanCM3, CMC2-CanCM4 e GEM-NEMO: "tmax" e "tmin"
+# NASA-GEOSS2S: "t2mmax" e "t2mmin"
+# NCAR-CESM1: Daily minimum of average 2-m temperature ("tsmn")
+#             Daily maximum of average 2-m temperature ("tsmx")
+# NCEP-CFSv2: Reference Temperature ("tref")
 
 
 # verificacao
