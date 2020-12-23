a19d6907acdc9cf2ba6e64d2720c691693786aeb
andreza-santos <andrezadantast@gmail.com>
2020-12-23 15:58:43 -0300

    referencias tabela

--- R/models-nmme.R
+++ R/models-nmme.R
@@ -8,7 +8,7 @@
 #              "NASA-GEOSS2S",
 #              "NCAR-CESM1",
 #              "NCEP-CFSv2")
-#variaveis <- c("prec", "sst")
+#variaveis <- c("prec", "tmax", "tmin", "t2mmax", "t2mmin", "tsmn", "tsmx", "tref")
 
 #tabela_control <- expand.grid(variaveis, modelos, anos) %>%
 #  as.data.frame() %>%
@@ -18,32 +18,41 @@
 #  arrange(modelo, var)
 
 # montar tabela de ano inicial e final de cada modelo
-models <- data.table(modelos = c("CanCM4i", "CanSIPSv2", "CMC1-CanCM3",
-                                 "CMC2-CanCM4", "GEM-NEMO", "NASA-GEOSS2S",
-                                 "NCAR-CESM1", "NCEP-CFSv2"),  
-                     ano_i = c("1981", "1981", "1981",  
-                               "1981", "1981", "1981",   
-                               "1980", "1982"),  
-                     ano_f = c("2018", "2018", "2010",
-                               "2010", "2018", "2017", 
-                               "2010", "2010"),
+library(data.table)
+models <- data.table(modelo = c("CanCM4i", "CanSIPSv2", "CMC1-CanCM3",
+                                "CMC2-CanCM4", "GEM-NEMO", "NASA-GEOSS2S",
+                                "NCAR-CESM1", "NCEP-CFSv2"),
+                     # centro = c("Canadian Meteorological Centre (CMC) – Canada",
+                     #            "Canadian Meteorological Centre (CMC) – Canada",
+                     #            "Canadian Meteorological Centre (CMC) – Canada",
+                     #            "Canadian Meteorological Centre (CMC) – Canada",
+                     #            "Recherche en Prévision Numérique (RPN)",
+                     #            "National Aeronautics and Space Administration (NASA) – United States",
+                     #            "National Center for Atmospheric Research (NCAR)",
+                     #            "National Centers for Environmental Prediction (NOAA/NCEP) – United States"),
+                     periodo = c("1981-2018", "1981-2018", "1981-2010",  
+                                 "1981-2010", "1981-2018", "1981-2017",   
+                                 "1980-2010", "1982-2010"),
                      n_membros = c(10, 20, 10,
-                                  10, 10, 4,
-                                  10, 24),
+                                   10, 10, 4,
+                                   10, 24),
                      lead = c("0.5-11.5", "0.5-11.5", "0.5-11.5",
                               "0.5-11.5", "0.5-11.5", "0.5-8.5",
-                              "0.5-11.5", "0.5-9.5")
+                              "0.5-11.5", "0.5-9.5"),
+                     referencia = c("Kirtman et al. (2014)",
+                                    "Kirtman et al. (2014)",
+                                    "Merryfield et al. (2013)",
+                                    "Merryfield et al. (2013)",
+                                    "Kirtman et al. (2014)",
+                                    "Vernieres et al. (2012)",
+                                    "Lawrence et al. (2012)",
+                                    "Saha et al. (2014)")
+                     # fase_i = c(""),
+                     # fase_ii = c(""),
+                     # variaveis = c("Precipitation (mm/day), Maximum Temperature (K), Minimum Temperature (K)", "", "", "", "","Precipitation (mm/day), Maximum Temperature (K), Minimum Temperature (K)", "Precipitation (mm/day), Daily minimum of average 2-m temperature (K), Daily maximum of average 2-m temperature (K)", "Precipitation (mm/day), Reference Temperature (K)"),
+                     # var_abrev = c("prec, tmax, tmin", "", "", "", "", "prec, t2mmax, t2mmin", "prec, tsmn, tsmx", "prec, tref")
 )
-
-# ver nomes das variaveis
-# Total Precipitation (mm/day): prec
-
-# CanCM4i, CanSIPSv2, CMC1-CanCM3, CMC2-CanCM4 e GEM-NEMO: "tmax" e "tmin"
-# NASA-GEOSS2S: "t2mmax" e "t2mmin"
-# NCAR-CESM1: Daily minimum of average 2-m temperature ("tsmn")
-#             Daily maximum of average 2-m temperature ("tsmx")
-# NCEP-CFSv2: Reference Temperature ("tref")
-
+models
 
 # verificacao
 #files_nc <- dir_ls("../output", glob = "*.nc")
